9d58899e3707599b1652ae84abf04b3570e76c40
Tanupreetk <bca_08829802023_tanupreet@vipstc.edu.in>
2025-08-05 01:18:27 +0530

    new commit

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './header.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
@@ -6,20 +6,28 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const Header =()=>{
     
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleScrollToOffer = () => {
-    navigate("/", { state: { scrollTo: "what-we-offer" } });
-  };
-
+  // const handleScrollToOffer = () => {
+  //   navigate("/", { state: { scrollTo: "what-we-offer" } });
+  // };
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu=()=>{
+    setIsMenuOpen(!isMenuOpen);
+  }
     return(
         
             <nav className="navbar">
                 <img id="logo" src="/logo.svg" alt="logo" />
-                <ul className="nav-bar">
+                <div className="hamburger" onClick={toggleMenu}>
+                  <div className={isMenuOpen ? "bar open" : "bar"}></div>
+                  <div className={isMenuOpen ? "bar open" : "bar"}></div>
+                  <div className={isMenuOpen ? "bar open" : "bar"}></div>
+                </div>
+                <ul className={`nav-bar ${isMenuOpen ? "slide-in": ""}`}>
                     <li><a href = "/"> Home</a></li>
                      
-    <li onClick={handleScrollToOffer}>What We Offer</li>
+    {/* <li onClick={handleScrollToOffer}>What We Offer</li> */}
                     <li><Link to = "/stories"> Stories</Link></li>
                     <li><Link to = "/Amenities"> Amenities</Link></li>
                     <li><Link to = "/Gallery">Gallery</Link></li>
